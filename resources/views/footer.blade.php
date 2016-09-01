4ff187c674e6ef5e30367d984f050ddd07849107
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2016-09-01 16:21:02 -0400

    Inlined footer elements

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
-<div class="footer">
-	<img src="/images/HW/badge_s.png" alt="badge logo" class="badgehw">
-	<p>Made with love by Purdue Hackers</p>
-	<a class="link-other" href="#"><img src="/images/HW/ph_logo.png" alt="Purdue Hackers" class="ph"></a>
-</div>
+<ul class="footer list-inline">
+	<li><img src="/images/HW/badge_s.png" alt="badge logo" class="badgehw"></li>
+	<li><p>Made with love by Purdue Hackers</p></li>
+	<li><a class="link-other" href="http://www.purduehackers.com/"><img src="/images/HW/ph_logo.png" alt="Purdue Hackers" class="ph"></a></li>
+</ul>
