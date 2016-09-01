05281afadbcd9cbf2161126f552ba9591085fe14
avincent6 <vincent6@purdue.edu>
2016-09-01 16:16:25 -0400

    footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="footer">
-	<img src="/images/HW/badge_s.png" alt="badge logo" class="badge">
+	<img src="/images/HW/badge_s.png" alt="badge logo" class="badgehw">
 	<p>Made with love by Purdue Hackers</p>
 	<a class="link-other" href="#"><img src="/images/HW/ph_logo.png" alt="Purdue Hackers" class="ph"></a>
 </div>
