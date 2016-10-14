15ceb4d64a6f0be46694228072f8654aafb77b54
avincent6 <vincent6@purdue.edu>
2016-10-14 09:24:52 -0400

    added resource page

--- resources/views/pages/resources.blade.php
+++ resources/views/pages/resources.blade.php
@@ -3,9 +3,121 @@
 @section("content")
 
 <div class="section"><div class='section-container'>
-	<h3>{{ env('ORG_NAME') }} Resources</h3>
-	<div class="panel panel-default">
-		<br><br>Yay, Resources!<br><br><br>
+	<div class="resources">
+	  <h1>Club Resources<h1>
+	    <p>The following links include material from all Hello World Workshops
+	       and other partnering club resources. </p>
+
+				 <div class="club-container ">
+				 	<div class="club clearfix ">
+				 		<div class="club-logo ">
+				 			<img src="images/HW/logo_final_s.png">
+				 		</div>
+				 		<div class="club-info clearfix">
+				 			<h2>Hello World Presents</h2>
+				 			<p class="subtext">Android, iOS, and WebDev Resources</p>
+				 			<a class="gitbook-link clearfix link-btn" href="https://wei170.gitbooks.io/learning_package_1-0/content/">
+				 				<img class="github-logo" alt="github" src="/images/HW/github.svg">
+				 				<p>See Resources</p>
+				 			</a>
+				 		</div>
+				 	</div>
+				 </div>
+
+	  <div class="club-container ">
+	    <div class="club clearfix ">
+	      <div class="club-logo ">
+	        <img src="images/HW/cswn.svg">
+	      </div>
+	      <div class="club-info clearfix">
+	        <h2>CSWN Presents</h2>
+	        <p class="subtext">Introduction to Angular Web Development</p>
+	        <a class="gitbook-link clearfix link-btn" href="https://thesoupgirl.gitbooks.io/angularjshelloworldtutorial/content/">
+	          <img class="github-logo" alt="github" src="/images/HW/github.svg">
+	          <p>Read Workshop</p>
+	        </a>
+	      </div>
+	    </div>
+	</div>
+
+	<div class="club-container ">
+		<div class="club clearfix ">
+			<div class="club-logo ">
+				<img src="images/HW/ios.svg">
+			</div>
+			<div class="club-info clearfix">
+				<h2>iOS Dev Club Presents</h2>
+				<p class="subtext">iOS Development with MapKit Workshop</p>
+				<a class="gitbook-link clearfix link-btn" href="https://deadlocked007.gitbooks.io/mapkit-gitbook/content/">
+					<img class="github-logo" alt="github" src="/images/HW/github.svg">
+					<p>Read Workshop</p>
+				</a>
+			</div>
+		</div>
+</div>
+<div class="club-container ">
+	<div class="club clearfix ">
+		<div class="club-logo ">
+			<img src="images/HW/webdev.svg">
+		</div>
+		<div class="club-info clearfix">
+			<h2>Web Dev Club Presents</h2>
+			<p class="subtext">Web Development Basics Workshop</p>
+			<a class="gitbook-link clearfix link-btn" href="https://maneesht.gitbooks.io/web-development-basics/content/">
+				<img class="github-logo" alt="github" src="/images/HW/github.svg">
+				<p>Read Workshop</p>
+			</a>
+		</div>
+	</div>
+</div>
+<div class="club-container ">
+	<div class="club clearfix ">
+		<div class="club-logo ">
+			<img src="images/HW/sigapp.svg">
+		</div>
+		<div class="club-info clearfix">
+			<h2>ACM Sigapp Presents</h2>
+			<p class="subtext">Hello Android</p>
+			<a class="gitbook-link clearfix link-btn" href="https://vidia.gitbooks.io/hello-android/content/">
+				<img class="github-logo" alt="github" src="/images/HW/github.svg">
+				<p>Read Workshop</p>
+			</a>
+		</div>
+	</div>
+</div>
+<div class="club-container ">
+	<div class="club clearfix ">
+		<div class="club-logo ">
+			<img src="images/HW/sigapp.svg">
+		</div>
+		<div class="club-info clearfix">
+			<h2>ACM Sigapp Presents</h2>
+			<p class="subtext">Hello Server</p>
+			<a class="gitbook-link clearfix link-btn" href="https://jwlehman93.gitbooks.io/hello-world-server/content/">
+				<img class="github-logo" alt="github" src="/images/HW/github.svg">
+				<p>Read Workshop</p>
+			</a>
+		</div>
+	</div>
+</div>
+<div class="club-container ">
+	<div class="club clearfix ">
+		<div class="club-logo ">
+			<img src="images/HW/bm.svg">
+		</div>
+		<div class="club-info clearfix">
+			<h2>BoilerMake Presents</h2>
+			<p class="subtext">Boilercamp</p>
+			<a class="gitbook-link clearfix link-btn" href="">
+				<img class="github-logo" alt="github" src="/images/HW/github.svg">
+				<p>Read Workshop</p>
+			</a>
+		</div>
+	</div>
+</div>
+
+
+	</div>
 	</div>
 </div></div>
 
