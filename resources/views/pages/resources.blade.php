d3e23e9e40f84ebd90bd53b0fd2dce3fb663940b
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2016-10-14 19:47:48 -0400

    Added live and dayof pages

--- resources/views/pages/resources.blade.php
+++ resources/views/pages/resources.blade.php
@@ -4,136 +4,138 @@
 
 <div class="section"><div class='section-container'>
 	<div class="resources">
-	  <h1>Club Resources<h1>
-	    <p>The following links include material from all Hello World Workshops
-	       and other partnering club resources. </p>
+		<h1>Hacker Resources<h1>
+		<p>The following links include material from all Hello World Workshops
+			and other partnering club resources.</p>
 
-				 <div class="club-container ">
-				 	<div class="club clearfix ">
-				 		<div class="club-logo ">
-				 			<img src="images/HW/logo_final_s.png">
-				 		</div>
-				 		<div class="club-info clearfix">
-				 			<h2>Hello World Presents</h2>
-				 			<p class="subtext">Android, iOS, and WebDev Resources</p>
-				 			<a class="gitbook-link clearfix link-btn" href="https://wei170.gitbooks.io/learning_package_1-0/content/">
-				 				<img class="github-logo" alt="github" src="/images/HW/github.svg">
-				 				<p>See Resources</p>
-				 			</a>
-				 		</div>
-				 	</div>
-				 </div>
-
-	  <div class="club-container ">
-	    <div class="club clearfix ">
-	      <div class="club-logo ">
-	        <img src="images/HW/cswn.svg">
-	      </div>
-	      <div class="club-info clearfix">
-	        <h2>CSWN Presents</h2>
-	        <p class="subtext">Introduction to Angular Web Development</p>
-	        <a class="gitbook-link clearfix link-btn" href="https://thesoupgirl.gitbooks.io/angularjshelloworldtutorial/content/">
-	          <img class="github-logo" alt="github" src="/images/HW/github.svg">
-	          <p>Read Workshop</p>
-	        </a>
-	      </div>
-	    </div>
-	</div>
-
-	<div class="club-container ">
-		<div class="club clearfix ">
-			<div class="club-logo ">
-				<img src="images/HW/ios.svg">
-			</div>
-			<div class="club-info clearfix">
-				<h2>iOS Dev Club Presents</h2>
-				<p class="subtext">iOS Development with MapKit Workshop</p>
-				<a class="gitbook-link clearfix link-btn" href="https://deadlocked007.gitbooks.io/mapkit-gitbook/content/">
-					<img class="github-logo" alt="github" src="/images/HW/github.svg">
-					<p>Read Workshop</p>
-				</a>
+		<div class="club-container">
+			<div class="club clearfix">
+				<div class="club-logo">
+					<img src="images/HW/logo_final_s.png">
+				</div>
+				<div class="club-info clearfix">
+					<h2>Hello World Presents</h2>
+					<p class="subtext">Android, iOS, and WebDev Resources</p>
+					<a class="gitbook-link clearfix link-btn" href="https://wei170.gitbooks.io/learning_package_1-0/content/">
+						<img class="github-logo" alt="github" src="/images/HW/github.svg">
+						<p>See Resources</p>
+					</a>
+				</div>
 			</div>
 		</div>
-</div>
-<div class="club-container ">
-	<div class="club clearfix ">
-		<div class="club-logo ">
-			<img src="images/HW/webdev.svg">
-		</div>
-		<div class="club-info clearfix">
-			<h2>Web Dev Club Presents</h2>
-			<p class="subtext">Web Development Basics Workshop</p>
-			<a class="gitbook-link clearfix link-btn" href="https://maneesht.gitbooks.io/web-development-basics/content/">
-				<img class="github-logo" alt="github" src="/images/HW/github.svg">
-				<p>Read Workshop</p>
-			</a>
-		</div>
-	</div>
-</div>
-<div class="club-container ">
-	<div class="club clearfix ">
-		<div class="club-logo ">
-			<img src="images/HW/sigapp.svg">
-		</div>
-		<div class="club-info clearfix">
-			<h2>ACM Sigapp Presents</h2>
-			<p class="subtext">Hello Android</p>
-			<a class="gitbook-link clearfix link-btn" href="https://vidia.gitbooks.io/hello-android/content/">
-				<img class="github-logo" alt="github" src="/images/HW/github.svg">
-				<p>Read Workshop</p>
-			</a>
-		</div>
-	</div>
-</div>
-<div class="club-container ">
-	<div class="club clearfix ">
-		<div class="club-logo ">
-			<img src="images/HW/sigapp.svg">
-		</div>
-		<div class="club-info clearfix">
-			<h2>ACM Sigapp Presents</h2>
-			<p class="subtext">Hello Server</p>
-			<a class="gitbook-link clearfix link-btn" href="https://jwlehman93.gitbooks.io/hello-world-server/content/">
-				<img class="github-logo" alt="github" src="/images/HW/github.svg">
-				<p>Read Workshop</p>
-			</a>
-		</div>
-	</div>
-</div>
-<div class="club-container ">
-	<div class="club clearfix ">
-		<div class="club-logo ">
-			<img src="images/HW/bm.svg">
-		</div>
-		<div class="club-info clearfix">
-			<h2>BoilerMake Presents</h2>
-			<p class="subtext">How to API</p>
-			<a class="gitbook-link clearfix link-btn" href="https://boilercamp.gitbooks.io/boilercamp2016/content/index.html">
-				<img class="github-logo" alt="github" src="/images/HW/github.svg">
-				<p>Read Workshop</p>
-			</a>
-		</div>
-	</div>
-</div>
 
-<div class="club-container ">
-	<div class="club clearfix ">
-		<div class="club-logo ">
-			<img src="images/HW/bm.svg">
+		<div class="club-container">
+	    	<div class="club clearfix">
+				<div class="club-logo">
+					<img src="images/HW/cswn.svg">
+	    		</div>
+				<div class="club-info clearfix">
+					<h2>CSWN Presents</h2>
+					<p class="subtext">Introduction to Angular Web Development</p>
+					<a class="gitbook-link clearfix link-btn" href="https://thesoupgirl.gitbooks.io/angularjshelloworldtutorial/content/">
+						<img class="github-logo" alt="github" src="/images/HW/github.svg">
+						<p>Read Workshop</p>
+		        	</a>
+	    		</div>
+	    	</div>
 		</div>
-		<div class="club-info clearfix">
-			<h2>BoilerMake Presents</h2>
-			<p class="subtext">How to WebDev</p>
-			<a class="gitbook-link clearfix link-btn" href="http://boilercamp.github.io/how-to-webdev/">
-				<img class="github-logo" alt="github" src="/images/HW/github.svg">
-				<p>Read Workshop</p>
-			</a>
+
+		<div class="club-container">
+			<div class="club clearfix">
+				<div class="club-logo">
+					<img src="images/HW/ios.svg">
+				</div>
+				<div class="club-info clearfix">
+					<h2>iOS Dev Club Presents</h2>
+					<p class="subtext">iOS Development with MapKit Workshop</p>
+					<a class="gitbook-link clearfix link-btn" href="https://deadlocked007.gitbooks.io/mapkit-gitbook/content/">
+						<img class="github-logo" alt="github" src="/images/HW/github.svg">
+						<p>Read Workshop</p>
+					</a>
+				</div>
+			</div>
 		</div>
-	</div>
-</div>
-
-
-	</div>
+		
+		<div class="club-container">
+			<div class="club clearfix">
+				<div class="club-logo">
+					<img src="images/HW/webdev.svg">
+				</div>
+				<div class="club-info clearfix">
+					<h2>Web Dev Club Presents</h2>
+					<p class="subtext">Web Development Basics Workshop</p>
+					<a class="gitbook-link clearfix link-btn" href="https://maneesht.gitbooks.io/web-development-basics/content/">
+						<img class="github-logo" alt="github" src="/images/HW/github.svg">
+						<p>Read Workshop</p>
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="club-container">
+			<div class="club clearfix">
+				<div class="club-logo">
+					<img src="images/HW/sigapp.svg">
+				</div>
+				<div class="club-info clearfix">
+					<h2>ACM Sigapp Presents</h2>
+					<p class="subtext">Hello Android</p>
+					<a class="gitbook-link clearfix link-btn" href="https://vidia.gitbooks.io/hello-android/content/">
+						<img class="github-logo" alt="github" src="/images/HW/github.svg">
+						<p>Read Workshop</p>
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="club-container">
+			<div class="club clearfix">
+				<div class="club-logo">
+					<img src="images/HW/sigapp.svg">
+				</div>
+				<div class="club-info clearfix">
+					<h2>ACM Sigapp Presents</h2>
+					<p class="subtext">Hello Server</p>
+					<a class="gitbook-link clearfix link-btn" href="https://jwlehman93.gitbooks.io/hello-world-server/content/">
+						<img class="github-logo" alt="github" src="/images/HW/github.svg">
+						<p>Read Workshop</p>
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="club-container">
+			<div class="club clearfix">
+				<div class="club-logo">
+					<img src="images/HW/bm.svg">
+				</div>
+				<div class="club-info clearfix">
+					<h2>BoilerMake Presents</h2>
+					<p class="subtext">How to API</p>
+					<a class="gitbook-link clearfix link-btn" href="https://boilercamp.gitbooks.io/boilercamp2016/content/index.html">
+						<img class="github-logo" alt="github" src="/images/HW/github.svg">
+						<p>Read Workshop</p>
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="club-container">
+			<div class="club clearfix">
+				<div class="club-logo">
+					<img src="images/HW/bm.svg">
+				</div>
+				<div class="club-info clearfix">
+					<h2>BoilerMake Presents</h2>
+					<p class="subtext">How to WebDev</p>
+					<a class="gitbook-link clearfix link-btn" href="http://boilercamp.github.io/how-to-webdev/">
+						<img class="github-logo" alt="github" src="/images/HW/github.svg">
+						<p>Read Workshop</p>
+					</a>
+				</div>
+			</div>
+		</div>
+		
 	</div>
 </div></div>
 
