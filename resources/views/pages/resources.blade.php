336202e22d4762c8137c45a7e6e682cb7326d019
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2016-10-14 19:52:48 -0400

    Open resources in new window

--- resources/views/pages/resources.blade.php
+++ resources/views/pages/resources.blade.php
@@ -16,7 +16,7 @@
 				<div class="club-info clearfix">
 					<h2>Hello World Presents</h2>
 					<p class="subtext">Android, iOS, WebDev, and other Intro Resources</p>
-					<a class="gitbook-link clearfix link-btn" href="https://wei170.gitbooks.io/learning_package_1-0/content/">
+					<a class="gitbook-link clearfix link-btn" href="https://wei170.gitbooks.io/learning_package_1-0/content/" target="_blank">
 						<img class="github-logo" alt="github" src="/images/HW/github.svg">
 						<p>See Resources</p>
 					</a>
@@ -32,7 +32,7 @@
 				<div class="club-info clearfix">
 					<h2>CSWN Presents</h2>
 					<p class="subtext">Introduction to Angular Web Development</p>
-					<a class="gitbook-link clearfix link-btn" href="https://thesoupgirl.gitbooks.io/angularjshelloworldtutorial/content/">
+					<a class="gitbook-link clearfix link-btn" href="https://thesoupgirl.gitbooks.io/angularjshelloworldtutorial/content/" target="_blank">
 						<img class="github-logo" alt="github" src="/images/HW/github.svg">
 						<p>Read Workshop</p>
 		        	</a>
@@ -48,7 +48,7 @@
 				<div class="club-info clearfix">
 					<h2>iOS Dev Club Presents</h2>
 					<p class="subtext">iOS Development with MapKit Workshop</p>
-					<a class="gitbook-link clearfix link-btn" href="https://deadlocked007.gitbooks.io/mapkit-gitbook/content/">
+					<a class="gitbook-link clearfix link-btn" href="https://deadlocked007.gitbooks.io/mapkit-gitbook/content/" target="_blank">
 						<img class="github-logo" alt="github" src="/images/HW/github.svg">
 						<p>Read Workshop</p>
 					</a>
@@ -64,7 +64,7 @@
 				<div class="club-info clearfix">
 					<h2>Web Dev Club Presents</h2>
 					<p class="subtext">Web Development Basics Workshop</p>
-					<a class="gitbook-link clearfix link-btn" href="https://maneesht.gitbooks.io/web-development-basics/content/">
+					<a class="gitbook-link clearfix link-btn" href="https://maneesht.gitbooks.io/web-development-basics/content/" target="_blank">
 						<img class="github-logo" alt="github" src="/images/HW/github.svg">
 						<p>Read Workshop</p>
 					</a>
@@ -80,7 +80,7 @@
 				<div class="club-info clearfix">
 					<h2>ACM Sigapp Presents</h2>
 					<p class="subtext">Hello Android</p>
-					<a class="gitbook-link clearfix link-btn" href="https://vidia.gitbooks.io/hello-android/content/">
+					<a class="gitbook-link clearfix link-btn" href="https://vidia.gitbooks.io/hello-android/content/" target="_blank">
 						<img class="github-logo" alt="github" src="/images/HW/github.svg">
 						<p>Read Workshop</p>
 					</a>
@@ -96,7 +96,7 @@
 				<div class="club-info clearfix">
 					<h2>ACM Sigapp Presents</h2>
 					<p class="subtext">Hello Server</p>
-					<a class="gitbook-link clearfix link-btn" href="https://jwlehman93.gitbooks.io/hello-world-server/content/">
+					<a class="gitbook-link clearfix link-btn" href="https://jwlehman93.gitbooks.io/hello-world-server/content/" target="_blank">
 						<img class="github-logo" alt="github" src="/images/HW/github.svg">
 						<p>Read Workshop</p>
 					</a>
@@ -112,7 +112,7 @@
 				<div class="club-info clearfix">
 					<h2>BoilerMake Presents</h2>
 					<p class="subtext">How to API</p>
-					<a class="gitbook-link clearfix link-btn" href="https://boilercamp.gitbooks.io/boilercamp2016/content/index.html">
+					<a class="gitbook-link clearfix link-btn" href="https://boilercamp.gitbooks.io/boilercamp2016/content/index.html" target="_blank">
 						<img class="github-logo" alt="github" src="/images/HW/github.svg">
 						<p>Read Workshop</p>
 					</a>
@@ -128,7 +128,7 @@
 				<div class="club-info clearfix">
 					<h2>BoilerMake Presents</h2>
 					<p class="subtext">How to WebDev</p>
-					<a class="gitbook-link clearfix link-btn" href="http://boilercamp.github.io/how-to-webdev/">
+					<a class="gitbook-link clearfix link-btn" href="http://boilercamp.github.io/how-to-webdev/" target="_blank">
 						<img class="github-logo" alt="github" src="/images/HW/github.svg">
 						<p>Read Workshop</p>
 					</a>
